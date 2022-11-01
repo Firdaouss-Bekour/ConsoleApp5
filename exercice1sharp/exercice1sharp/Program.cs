07b91056939fc7d2078b1664bc819a8314830d5b
Firdaouss-Bekour <116847659+Firdaouss-Bekour@users.noreply.github.com>
2022-11-01 19:19:23 +0100

    Add files via upload

---
+++ exercice1sharp/exercice1sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercice1sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbr;
+            Console.WriteLine("veuillez entrez un nbr");
+            nbr = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= nbr / 2; i++)
+            {
+                if (nbr % i == 0)
+                {
+                    Console.WriteLine(nbr + " n'est pas premier");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine(nbr + "est un nbr premier ");
+            Console.ReadLine();
+        }
+    }
+}
+
